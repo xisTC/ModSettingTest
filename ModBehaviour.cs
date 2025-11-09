6455d137e0e94b03cfb0009e254259961b1086f5
xisTC <536440296@qq.com>
2025-11-09 16:58:28 +0800

    测试语言切换更新UI

--- ModBehaviour.cs
+++ ModBehaviour.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using Duckov.Modding;
+using SodaCraft.Localizations;
 using UnityEngine;
 
 namespace ModSettingTest {
@@ -7,11 +8,14 @@ namespace ModSettingTest {
         private void OnEnable() {
             ModManager.OnModActivated += ModManager_OnModActivated;
             ModManager.OnModWillBeDeactivated += ModManager_OnModWillBeDeactivated;
+            LocalizationManager.OnSetLanguage += LocalizationManager_OnSetLanguage;
             Saver.Load();
         }
+
         private void OnDisable() {
             ModManager.OnModActivated -= ModManager_OnModActivated;
             ModManager.OnModWillBeDeactivated -= ModManager_OnModWillBeDeactivated;
+            LocalizationManager.OnSetLanguage -= LocalizationManager_OnSetLanguage;
             Saver.Save();
             Setting.Clear();
         }
@@ -30,21 +34,21 @@ namespace ModSettingTest {
             }
         }
 
-        private void AddUI() {
-            ModSettingAPI.AddDropdownList("D1", "下拉列表1",
-                new List<string> { "选项1", "选项2", "选项3" }, 
+        private void AddUI(SystemLanguage language=SystemLanguage.ChineseSimplified) {
+            ModSettingAPI.AddDropdownList("D1", language+":下拉列表1",
+                new List<string> { language+":选项1", language+":选项2", language+":选项3" }, 
                 Setting.Dropdown1, Setting.SetDropdown1);
-            ModSettingAPI.AddDropdownList("D2", "下拉列表2", 
-                new List<string> { "选项7", "选项8", "选项9" },
+            ModSettingAPI.AddDropdownList("D2", language+":下拉列表2", 
+                new List<string> { language+":选项7", language+":选项8", language+":选项9" },
                 Setting.Dropdown2, Setting.SetDropdown2);
-            ModSettingAPI.AddToggle("T1", "开关1", Setting.Toggle1, Setting.SetToggle1);
-            ModSettingAPI.AddToggle("T2", "开关2", Setting.Toggle2, Setting.SetToggle2);
+            ModSettingAPI.AddToggle("T1", language+":开关1", Setting.Toggle1, Setting.SetToggle1);
+            ModSettingAPI.AddToggle("T2", language+":开关2", Setting.Toggle2, Setting.SetToggle2);
             ModSettingAPI.AddSlider("S1", "滑块1", Setting.Slider1, new Vector2(0, 100), Setting.SetSlider1);
             ModSettingAPI.AddSlider("S2", "滑块2", Setting.Slider2, new Vector2(0, 1000), Setting.SetSlider2,2);
             ModSettingAPI.AddSlider("S3", "滑块3", 60, new Vector2(0, 1000), null,3,8);
             ModSettingAPI.AddSlider("S4", "滑块4", 50,0,200,value=>{ Debug.Log("滑块4:"+value);});
-            ModSettingAPI.AddInput("I1", "输入框1", Setting.Input1, 40, Setting.SetInput1);
-            ModSettingAPI.AddInput("I2", "输入框2", Setting.Input2, 50, Setting.SetInput2);
+            ModSettingAPI.AddInput("I1", language+":输入框1", Setting.Input1, 40, Setting.SetInput1);
+            ModSettingAPI.AddInput("I2", language+":输入框2", Setting.Input2, 50, Setting.SetInput2);
             ModSettingAPI.AddKeybinding("K1", "按键绑定1", Setting.Keybinding1, Setting.SetKeybinding1);
             ModSettingAPI.AddKeybinding("K2", "按键绑定2", Setting.Keybinding2, Setting.SetKeybinding2);
             //设置按键绑定默认值
@@ -61,9 +65,9 @@ namespace ModSettingTest {
                 ModSettingAPI.RemoveUI("G3");
             });
             //Group用于整理设置项过多的情况，一般来说不需要使用。
-            ModSettingAPI.AddGroup("G1", "下拉列表组", new List<string>() { "D1", "D2" },0.6f,true);
-            ModSettingAPI.AddGroup("G2", "开关组", new List<string>() { "T1", "T2" }, 0.7f,true,true);
-            ModSettingAPI.AddGroup("G3", "滑块组", new List<string>() { "S1", "S2","S3","S4" }, 0.7f,true);
+            ModSettingAPI.AddGroup("G1", language+":下拉列表组", new List<string>() { "D1", "D2" },0.6f,true);
+            ModSettingAPI.AddGroup("G2", language+":开关组", new List<string>() { "T1", "T2" }, 0.7f,true,true);
+            ModSettingAPI.AddGroup("G3", language+":滑块组", new List<string>() { "S1", "S2","S3","S4" }, 0.7f,true);
             // ModSettingAPI.AddGroup("G4", "输入组", new List<string>() { "I1", "I2"}, 0.7f,false,true);
             // ModSettingAPI.AddGroup("G5", "绑定组", new List<string>() { "K1", "K2","K3","K4"});
             // ModSettingAPI.AddGroup("G6", "按钮组", new List<string>() { "B1", "B2","B3"});
@@ -71,6 +75,7 @@ namespace ModSettingTest {
             Setting.OnSlider1ValueChanged += Setting_OnSlider1ValueChanged;
             
         }
+
         private void Reset() {
             //注意：SetValue只是单方面通知UI设置值,也就是说UI的onValueChange不会被调用
             //如果需要同步，应该先设置此mod的值，再将此mod的值设置给ModSetting。如：Dropdown1这样，其余的都只改变了UI的值并没有改变此mod的值。
@@ -95,6 +100,13 @@ namespace ModSettingTest {
             //此mod设置数据变化时，设置UI的数值，单方面通知UI更新
             ModSettingAPI.SetValue("S1", value);
         }
+        // 语言切换执行：销毁原来的UI，添加新UI。mod标题会从mod设置的底部添加
+        // 如果不想改变mod标题位置，可以使用RmoveUI(key)后，添加相应的UI
+        // 如果觉得重新实例化会损失性能，后续更新UpdateUI(key)类似的API，或者使用对象池，或者RegisterLanguagePack(..)由ModSetting来更新UI
+        private void LocalizationManager_OnSetLanguage(SystemLanguage obj) {
+            ModSettingAPI.RemoveMod();
+            AddUI(obj);
+        }
 
         private void ModManager_OnModWillBeDeactivated(ModInfo arg1, Duckov.Modding.ModBehaviour arg2) {
             if (arg1.name != ModSettingAPI.MOD_NAME || !ModSettingAPI.Init(info)) return;
@@ -105,11 +117,11 @@ namespace ModSettingTest {
         private void ModManager_OnModActivated(ModInfo arg1, Duckov.Modding.ModBehaviour arg2) {
             if (arg1.name != ModSettingAPI.MOD_NAME || !ModSettingAPI.Init(info)) return;
             //(触发时机:此mod在ModSetting之前启用)检查启用的mod是否是ModSetting,是进行初始化
-            AddUI();
+            AddUI(LocalizationManager.CurrentLanguage);
         }
         protected override void OnAfterSetup() {
             //(触发时机:此mod在ModSetting之后启用)此mod，Setup后,尝试进行初始化
-            if (ModSettingAPI.Init(info)) AddUI();
+            if (ModSettingAPI.Init(info)) AddUI(LocalizationManager.CurrentLanguage);
         }
     }
 }
